bc0606a5088a74bc5603c84455962a11e833a7f6
visualstudio <visualstudio@122-PC38>
2019-03-08 18:09:12 -0500

    Lab1

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Name: Chetan rahanoo
+// Student id: C0732135
+//CSD3354 Section 2
+//Assignment 2
+//March 6,2019
 
 namespace Assignment2
 {
@@ -10,6 +15,23 @@ namespace Assignment2
     {
         static void Main(string[] args)
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
         }
+    }
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+
     }
 }
